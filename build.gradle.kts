e14c32746ff8c61c63a6cac764e13b2d1d2fabae
youngzuhwang <dghkddudwn@kaist.ac.kr>
2024-06-03 23:12:29 +0900

    Change build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
-    //Ktlint 의존성
+    // Ktlint 의존성
     id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
@@ -33,12 +33,12 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    //Kotest 의존성
+    // Kotest 의존성
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
     testImplementation("io.kotest:kotest-assertions-core:5.6.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("io.kotest:kotest-property:5.6.2")
-    //Swagger 의존성
+    // Swagger 의존성
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     // queryDSL 의존성
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
@@ -54,4 +54,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
 }
