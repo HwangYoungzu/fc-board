503be1e28c6f168607e4b3b9ad5f488ea9d61b39
youngzuhwang <dghkddudwn@kaist.ac.kr>
2024-06-03 22:55:57 +0900

    fc : Configure application.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    runtimeOnly("com.h2database:h2")
+    // MySQL 연동위해 runtimeOnly->testImplementation 변경
+    testImplementation("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
