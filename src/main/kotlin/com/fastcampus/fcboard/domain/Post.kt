763d536bf4a9dc670f2977e2a5021cc602250196
youngzuhwang <dghkddudwn@kaist.ac.kr>
2024-06-15 20:45:50 +0900

    feat : Design API Domain for Comments

--- src/main/kotlin/com/fastcampus/fcboard/domain/Post.kt
+++ src/main/kotlin/com/fastcampus/fcboard/domain/Post.kt
@@ -2,10 +2,12 @@ package com.fastcampus.fcboard.domain
 
 import com.fastcampus.fcboard.exception.PostNotUpdatableException
 import com.fastcampus.fcboard.service.dto.PostUpdateRequestDto
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 
 @Entity
 class Post(
@@ -23,6 +25,14 @@ class Post(
         private set
     var content: String = content
         private set
+
+    // 고아객체는 부모 엔티티와 연관관계가 끊어진 자식 엔티티를 가리킵니다.
+    // 부모가 제거될때, 부모와 연관되어있는 모든 자식 엔티티들은 고아객체가 됩니다.
+    // 부모 엔티티와 자식 엔티티 사이의 연관관계를 삭제할때, 해당 자식 엔티티는 고아객체가 됩니다.
+    // orphanRemoval = true 로 하면, 이 고아객체들을 자동으로 삭제 처리해줍니다
+    @OneToMany(mappedBy = "post", orphanRemoval = true, cascade = [CascadeType.ALL])
+    var comments: MutableList<Comment> = mutableListOf()
+        protected set
 
     fun update(postUpdateRequestDto: PostUpdateRequestDto) {
         if (postUpdateRequestDto.updatedBy != this.createdBy) {
